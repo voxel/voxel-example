49abd510ac3d5adf98059d27e92ac553d1709e9b
deathcap <deathcap@gmx.co.uk>
2014-04-13 21:26:33 -0700

    Add required plugins
    Blocks show up, but they aren't what we set (no connection from the
    voxel-engine's voxel generator/events to game-shell-voxel's meshing and
    rendering yet).

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-require('./')()
+require('voxel-registry')
+require('voxel-stitch')
+require('./lib/blocks') // TODO: refactor with game-shell-voxel
+
+require('./')({require: require, pluginOpts: {
+  'voxel-registry': {},
+  'voxel-stitch': {debug: false},
+  './lib/blocks': {}
+}})
