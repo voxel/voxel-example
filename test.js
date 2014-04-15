2fef378e43b3e58a35969f407821bc32c1e114aa
deathcap <deathcap@gmx.co.uk>
2014-04-15 12:58:45 -0700

    Remove now-builtin plugins

--- test.js
+++ test.js
@@ -1,9 +1,5 @@
-require('voxel-registry')
-require('voxel-stitch')
 require('./lib/blocks') // TODO: refactor with game-shell-voxel
 
 require('./')({require: require, pluginOpts: {
-  'voxel-registry': {},
-  'voxel-stitch': {debug: false},
   './lib/blocks': {}
 }})
