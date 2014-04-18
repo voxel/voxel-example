25e90a492ea80dbe8386f6d386bacca55d687662
deathcap <deathcap@gmx.co.uk>
2014-04-17 21:21:29 -0700

    Adjust initial camera rotation for showChunk() example

--- demo.js
+++ demo.js
@@ -4,4 +4,5 @@ require('./lib/terrain.js')
 require('./')({require: require, pluginOpts: {
   './lib/blocks.js': {},
   './lib/terrain.js': {},
+  'game-shell-fps-camera': {position: [28,-37,27], rotationX:20*Math.PI/180, rotationY:133*Math.PI/180},
 }})
