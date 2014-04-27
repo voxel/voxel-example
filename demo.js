df701115555cc6b938037438801c066f0e351931
deathcap <deathcap@gmx.co.uk>
2014-04-27 16:38:04 -0700

    Add voxel-chunkborder, hit F9 to toggle chunk debug borders
    Update voxel-engine
    
    deathcap/voxel-engine@5746b254c8af731303449b3037620f0561329089 Update dependencies - voxel-mesher/shader ^0.9.0
    deathcap/voxel-engine@9f2b57bbf0970960effd4dc755b29fb7edc9d765 Move cameraPosition negation to game-shell-fps-camera getPosition()
    
    Commit message generated by https://github.com/deathcap/lmno-cl

--- demo.js
+++ demo.js
@@ -9,9 +9,11 @@ require('voxel-drop')
 require('voxel-keys')
 require('voxel-artpacks')
 require('voxel-wireframe')
+require('voxel-chunkborder')
 
 createExample({require: require, pluginOpts: {
   'game-shell-fps-camera': {position: [28,-37,27], rotationX:20*Math.PI/180, rotationY:133*Math.PI/180},
+  //'game-shell-fps-camera': {position: [-35,-7,-87], rotationX:0.09959166153309228, rotationY:5.785302284120946},
   //'voxel-stitch': {debug: true},
 
   './lib/blocks.js': {},
@@ -22,4 +24,5 @@ createExample({require: require, pluginOpts: {
   'voxel-keys': {},
   'voxel-artpacks': {},
   'voxel-wireframe': {},
+  'voxel-chunkborder': {},
 }})
