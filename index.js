8fffe1468a7997f195288ef5c9320978ed612465
deathcap <deathcap@gmx.co.uk>
2016-01-18 21:22:46 -0800

    Remove terrain generator in favor of voxel-flatland
    TODO: move the original generator (has a demonstration of glass, and
    some other more complex generation, but like voxel-land) to another
    plugin, https://github.com/deathcap/voxel-example/issues/6

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ var main = function() {
   createEngine({pluginLoaders: {
     './lib/blocks.js': require('./lib/blocks.js'),
     'voxel-flatland': require('voxel-flatland'),
-    //'./terrain.js: require('./lib/terrain.js'),
   }, exposeGlobal: true, pluginOpts: {
     'game-shell-fps-camera': {position: [-4, -40, -5], rotationX:15*Math.PI/180, rotationY:135*Math.PI/180 },
 
     './lib/blocks.js': {},
     'voxel-flatland': {block: 'bedrock'},
-
-    //'./lib/terrain.js': {}
   }})
 };
 
