83949a10a9a7fe0d195ea9b978a66717715cb450
deathcap <deathcap@gmx.co.uk>
2016-01-18 21:41:43 -0800

    Use block from voxel-bedrock plugin

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var createEngine = require('voxel-engine-stackgl')
 
 var main = function() {
   createEngine({pluginLoaders: {
-    './lib/blocks.js': require('./lib/blocks.js'),
+    'voxel-bedrock': require('voxel-bedrock'),
     'voxel-flatland': require('voxel-flatland'),
   }, exposeGlobal: true, pluginOpts: {
     'game-shell-fps-camera': {position: [-4, -40, -5], rotationX:15*Math.PI/180, rotationY:135*Math.PI/180 },
 
-    './lib/blocks.js': {},
+    'voxel-bedrock': {},
     'voxel-flatland': {block: 'bedrock'},
   }})
 };
