cfd881b29f6f090974d3a7e67696aeff9e4fce9a
deathcap <deathcap@gmx.co.uk>
2016-01-18 21:48:46 -0800

    Simplify example.js from demo.js/index.js

---
+++ example.js
@@ -0,0 +1,13 @@
+'use strict';
+
+require('voxel-engine-stackgl')({
+  pluginLoaders: {
+    'voxel-bedrock': require('voxel-bedrock'),
+    'voxel-flatland': require('voxel-flatland')
+  },
+  pluginOpts: {
+    'game-shell-fps-camera': {position: [-4, -40, -5], rotationX:15*Math.PI/180, rotationY:135*Math.PI/180 },
+
+    'voxel-bedrock': {},
+    'voxel-flatland': {block: 'bedrock'}
+  }});
